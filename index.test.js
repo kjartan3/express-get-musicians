2397d5c7bf2012ba981fa2bfbbcbd7010f38680b
Kjartan Heimisson <kheimisson@gmail.com>
2024-11-04 18:15:58 +0000

    added GET route for /musician and returned all musicians on localhost

--- index.test.js
+++ index.test.js
@@ -12,6 +12,22 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test('testing musician endpoint', async () => {
+        const response = await request(app).get('/musician');
+    });
+
+    test('testing musician endpoint', async () => {
+        const response = await request(app).get('/musician');
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('testing musician endpoint', async () => {
+        const response = await request(app).get('/musician');
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(responseData)).toBe(true);
+
+    });
     
     
 
