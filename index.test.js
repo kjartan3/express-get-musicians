660668b98eec076396ef50a99465c53c3a9c648c
Kjartan Heimisson <kheimisson@gmail.com>
2024-11-05 14:55:19 +0000

    added CRUD actions with tests

--- index.test.js
+++ index.test.js
@@ -13,26 +13,48 @@ const {seedMusician} = require("./seedData");
 describe('./musicians endpoint', () => {
     // Write your tests here
     test('testing musician endpoint', async () => {
-        const response = await request(app).get('/musician');
+        const response = await request(app).get('/musicians');
     });
 
-    test('testing musician endpoint', async () => {
-        const response = await request(app).get('/musician');
+    test('testing musician endpoint 2', async () => {
+        const response = await request(app).get('/musicians');
         expect(response.statusCode).toBe(200);
     });
 
-    test('testing musician endpoint', async () => {
-        const response = await request(app).get('/musician');
+    test('testing musician endpoint 3', async () => {
+        const response = await request(app).get('/musicians');
         const responseData = JSON.parse(response.text);
         expect(response.statusCode).toBe(200);
         expect(Array.isArray(responseData)).toBe(true);
 
     });
-    
-    
 
+    test('GET /musicians/:id', async () => {
+        const response = await request(app).get('/musicians/1');
+        expect(response.status).toBe(200);
+    });
 
+    test('return 404 if musician not found', async () => {
+        const response = await request(app).get('/musicians/999');
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual({ error: 'Musician not found' });
+    });
 
+    test('POST /musicians', async () => {
+        const testEntry = { name: 'John Doe', instrument: 'Drum' };
+        const response = await request(app).post('/musicians').send(testEntry);
+        expect(response.status).toBe(200);
+    });
 
-    
-})
+    test('PUT /musicians/:id', async () => {
+        const updatedTestEntry = { name: 'Jon Doe', instrument: 'Drum' };
+        const response = await request(app).put('/musicians/:id').send(updatedTestEntry);
+        expect(response.status).toBe(200);
+    });
+
+    test('DELETE /musicians/:id', async () => {
+        const deletedTestEntry = {};
+        const response = await request(app).delete('/musicians/:id').send(deletedTestEntry);
+        expect(response.status).toBe(200);
+    })
+});
--- src/app.js
+++ src/app.js
@@ -6,10 +6,38 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/musician', async (req, res) => {
+app.get('/musicians', async (req, res) => {
     const musicians = await Musician.findAll({});
     res.json(musicians);
+})
+
+app.get('/musicians/:id', async (req, res) => {
+    id = req.params.id;
+    const findMusician = await Musician.findByPk(id);
+
+    if (!findMusician) {
+        return res.status(404).json({ error: 'Musician not found' });
+    }
+
+    res.json(findMusician);
+})
+
+app.post('/musicians', async (req, res) => {
+    const musician = await Musician.create(req.body);
+    res.json(musician);
+})
+
+app.put('/musicians/:id', async (req, res) => {
+    const updatedMusician = await Musician.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedMusician);
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    const deletedMusician = await Musician.destroy({where: {id: req.params.id}});
+    res.json(deletedMusician);
 })
 
 
